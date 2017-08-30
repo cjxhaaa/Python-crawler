e740b1cbbac376cdd4d1d86389028b6ac9d2f434
cjxh <405019079@qq.com>
2017-08-30 22:04:11 +0800

    selenium.taobao

--- selenium抓取淘宝/test.py
+++ selenium抓取淘宝/test.py
@@ -1,19 +1,30 @@
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver.support.wait import WebDriverWait
+# from pprint import pprint
+
+# browser = webdriver.Chrome()
+# try:
+#     browser.get('https://www.baidu.com')
+#     input = browser.find_element_by_id('kw')
+#     input.send_keys('Python')
+#     input.send_keys(Keys.ENTER)
+#     wait = WebDriverWait(browser, 10)
+#     wait.until(EC.presence_of_element_located((By.ID, 'content_left')))
+#     # print(browser.current_url)
+#     # print(browser.get_cookies())
+#     # print(browser.page_source)
+#     with open('baidu.txt','w',encoding='utf-8') as b:
+#         b.write(browser.page_source)
+# finally:
+#     browser.close()
+
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
 
 browser = webdriver.Chrome()
-try:
-    browser.get('https://www.baidu.com')
-    input = browser.find_element_by_id('kw')
-    input.send_keys('Python')
-    input.send_keys(Keys.ENTER)
-    wait = WebDriverWait(browser, 10)
-    wait.until(EC.presence_of_element_located((By.ID, 'content_left')))
-    print(browser.current_url)
-    print(browser.get_cookies())
-    print(browser.page_source)
-finally:
-    browser.close()
+browser.get('https://www.taobao.com')
+with open('taobao.html','w',encoding='utf-8') as b:
+    b.write(browser.page_source)
+browser.close()
