c3c07bed470653fef46fd592622603f548801cf6
cjxh <405019079@qq.com>
2017-09-10 23:28:41 +0800

    更新可按时间查询

--- train_spider/run.py
+++ train_spider/run.py
@@ -1,13 +1,9 @@
-from tickets import SearchTickets,SearchDetailTickets
+from tickets import SearchTickets
 
 
 if __name__ == '__main__':
 	text = input('请输入您要查询的列车信息：')
 	t = str(text).split(' ')
 	print(t)
-	if len(t) == 4:
-		S = SearchDetailTickets(text)
-		print(S.get_train_info())
-	elif len(t) == 3:
-		S = SearchTickets(text)
-		print(S.get_train_info())
+	S = SearchTickets(text)
+	print(S.get_train_info())
