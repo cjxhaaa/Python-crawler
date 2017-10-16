07b0714b0cba09bb32235e367b90acf965a29cb0
cjxh <405019079@qq.com>
2017-10-16 22:03:41 +0800

    g

---
+++ sinaweibo/sinaweibo/spiders/weibo.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from scrapy import Spider,FormRequest
+
+
+class WeiboSpider(Spider):
+    name = 'weibo'
+    allowed_domains = ['weibo.cn']
+    search_url = 'https://weibo.cn/search/mblog'
+    max_page = 100
+
+    def start_requests(self):
+        keyword = '000001'
+        url = '{url}?keyword={keyword}'.format(url=self.search_url, keyword=keyword)
+        for page in range(self.max_page + 1):
+            data = {
+                'mp': str(self.max_page),
+                'page': str(page)
+            }
+            yield FormRequest(url,callback=self.parse_index,formdata=data)
+
+    def parse_index(self, response):
+        print(response.text)
--- 官方接口爬取微信公众号文章/wechat_spider.py
+++ 官方接口爬取微信公众号文章/wechat_spider.py
@@ -14,6 +14,7 @@ def get_token():
     response = requests.get(url,cookies=cookies)
     # token = re.search(r'token=\d+',response.url).group()
     # print(token)
+    print(response.url)
     token = re.findall(r'token=(\d+)',response.url)[0]
     return token
 
